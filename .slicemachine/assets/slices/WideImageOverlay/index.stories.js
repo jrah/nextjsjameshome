5972f50602467b3703b3c52c342fde2e19a2b3ee
James Home <james@jameshome.co.uk>
2021-07-11 01:50:53 +0100

    init commit

---
+++ .slicemachine/assets/slices/WideImageOverlay/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/WideImageOverlay';
+
+export default {
+  title: 'slices/WideImageOverlay'
+}
+
+
+export const _DefaultSlice = () => <MyComponent slice={{"id":"_DefaultSlice","name":"Default slice","docURL":"...","version":"sktwi1xtmkfgx8626","description":"WideImageOverlay","primary":{"title":[{"type":"heading1","text":"This is where it all begins...","spans":[]}],"description":[{"type":"paragraph","text":"start by editing this slice from inside the SliceMachine builder!","spans":[]}]},"slice_type":"wide_image_overlay"}} />
+_DefaultSlice.storyName = 'Default slice'

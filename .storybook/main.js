b1c6905d00706a2bd6f25eb110cad668121ec0a2
James Home <james@jameshome.co.uk>
2021-07-14 20:27:04 +0100

    Fix tailwind not appearing in slices on storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,5 @@
 const { getStoriesPaths } = require('slice-machine-ui/helpers/storybook')
 module.exports = {
-  stories: [...getStoriesPaths(), /*...*/]
+  stories: [...getStoriesPaths(), /*...*/],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-postcss'],
 }

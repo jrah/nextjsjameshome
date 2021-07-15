e8c328021c3db6c3006f959d71113ec2db8cff3f
James Home <james@jameshome.co.uk>
2021-07-15 18:28:37 +0100

    Update to show Prismic People

--- slices/WideImageOverlay/index.js
+++ slices/WideImageOverlay/index.js
@@ -3,32 +3,24 @@ import { RichText } from 'prismic-reactjs';
 
 const MySlice = ({ slice }) => (
   <section>
-    <span className="title">
+    <span className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
       {
         slice.primary.title ?
         <RichText render={slice.primary.title}/>
         : <h2>Template slice, update me!</h2>
       }
     </span>
+    <img src={slice.primary.image.url} alt={slice.primary.image.alt} />
+    <div className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
     {
       slice.primary.description ?
       <RichText render={slice.primary.description}/>
       : <p>start by editing this slice from inside the SliceMachine builder!</p>
     }
-    {slice?.items?.map((item, i) => 
-    <img src={item.image.url}/>
-    )}
-    <style jsx>{`
-        section {
-          max-width: 600px;
-          margin: 4em auto;
-          text-align: center;
-        }
-        .title {
-          color: #8592e0;
-        }
-    `}</style>
-  </section>
+    </div>
+    { slice?.items?.map((item, i) => <div key={i} className={`gallery ${slice.variation}-gallery`}><RichText render={item.heading} /><img src={item.image.url} alt={item.image.alt} /> <span >{ item.test }</span></div>) }
+    {/* { slice?.items?.map((item, i) => <img src={item.image.url} alt={item.image.alt} />) }   */}
+    </section>
 );
 
 export default MySlice;

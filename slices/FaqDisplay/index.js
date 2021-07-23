3c5b6324950a76add5743dd1a09764b72e1747d8
James Home <james@jameshome.co.uk>
2021-07-23 16:52:03 +0100

    Add slice variation

--- slices/FaqDisplay/index.js
+++ slices/FaqDisplay/index.js
@@ -17,35 +17,70 @@ const Accordion = ({ children, title, isExpand = false }) => {
 };
 
 const FaqDisplay = ({ slice }) => {
-  return (
-    <section>
-      <span className="text-center text-3xl font-extrabold text-gray-900 sm:text-4xl">
-        {slice.primary.title ? (
-          <RichText render={slice.primary.title} />
-        ) : (
-          <h2>Template slice, update me!</h2>
-        )}
-      </span>
-      <div className="text-center mt-1">
-        {slice.primary.description ? (
-          <RichText render={slice.primary.description} />
-        ) : (
-          <p>start by editing this slice from inside Prismic builder!</p>
-        )}
-      </div>
-      <div className="mx-auto max-w-3xl mt-4 border-t-2">
-        {slice?.items?.map((item, i) => (
-          <div key={i} className="py-6 border-b">
-            <Accordion title={item.heading}>
-              <div className="mt-2 text-gray-700">
-                <RichText render={item.content} />
+  if (slice.variation === "staticFaq") {
+    return (
+      <section className="mx-auto max-w-5xl">
+        <span className="text-center text-3xl font-extrabold text-gray-900 sm:text-4xl">
+          {slice.primary.title ? (
+            <RichText render={slice.primary.title} />
+          ) : (
+            <h2>Template slice, update me!</h2>
+          )}
+        </span>
+        <div className="text-center mt-1">
+          {slice.primary.description ? (
+            <RichText render={slice.primary.description} />
+          ) : (
+            <p>start by editing this slice from inside Prismic builder!</p>
+          )}
+        </div>
+        <div className="mt-4 grid grid-rows-1 grid-flow-col gap-12">
+          {slice?.items?.map((item, i) => (
+            <div key={i} className="py-6">
+              <div>
+                <div className="font-semibold">
+                  <RichText render={item.heading} />
+                </div>
+                <div className="mt-2 text-gray-700">
+                  <RichText render={item.content} />
+                </div>
               </div>
-            </Accordion>
-          </div>
-        ))}
-      </div>
-    </section>
-  );
+            </div>
+          ))}
+        </div>
+      </section>
+    );
+  } else {
+    return (
+      <section>
+        <span className="text-center text-3xl font-extrabold text-gray-900 sm:text-4xl">
+          {slice.primary.title ? (
+            <RichText render={slice.primary.title} />
+          ) : (
+            <h2>Template slice, update me!</h2>
+          )}
+        </span>
+        <div className="text-center mt-1">
+          {slice.primary.description ? (
+            <RichText render={slice.primary.description} />
+          ) : (
+            <p>start by editing this slice from inside Prismic builder!</p>
+          )}
+        </div>
+        <div className="mx-auto max-w-3xl mt-4 border-t-2">
+          {slice?.items?.map((item, i) => (
+            <div key={i} className="py-6 border-b">
+              <Accordion title={item.heading}>
+                <div className="mt-2 text-gray-700">
+                  <RichText render={item.content} />
+                </div>
+              </Accordion>
+            </div>
+          ))}
+        </div>
+      </section>
+    );
+  }
 };
 
 export default FaqDisplay;

6465a974c664c05e4cdd1c4db5a31363a74d578c
James Home <james@jameshome.co.uk>
2021-07-27 02:12:09 +0100

    Update wide image overlay to divide between

--- slices/WideImageOverlay/index.js
+++ slices/WideImageOverlay/index.js
@@ -1,28 +1,34 @@
-import React from 'react';
-import { RichText } from 'prismic-reactjs';
+import React from "react";
+import { RichText } from "prismic-reactjs";
 
 const MySlice = ({ slice }) => (
-  <section>
-    <span className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-      {
-        slice.primary.title ?
-        <RichText render={slice.primary.title}/>
-        : <h2>Template slice, update me!</h2>
-      }
-    </span>
-    {
-      slice.primary.image ?
-    <img src={slice.primary.image.url} alt={slice.primary.image.alt} /> : null }
-    <div className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
-    {
-      slice.primary.description ?
-      <RichText render={slice.primary.description}/>
-      : <p>start by editing this slice from inside the SliceMachine builder!</p>
-    }
+  <section className="mx-auto max-w-5xl">
+    <div className="flex items-center">
+      <div>
+        <span className="mt-4 text-4xl tracking-tight font-extrabold text-black sm:mt-5 sm:text-6xl lg:mt-6 xl:text-6xl">
+          {slice.primary.title ? (
+            <RichText render={slice.primary.title} />
+          ) : (
+            <h2>Template slice, update me!</h2>
+          )}
+        </span>
+        <div className="mt-3 text-base text-gray-800 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl">
+          {slice.primary.description ? (
+            <RichText render={slice.primary.description} />
+          ) : (
+            <p>
+              start by editing this slice from inside the SliceMachine builder!
+            </p>
+          )}
+        </div>
+      </div>
+      <div>
+      {slice.primary.image ? (
+          <img src={slice.primary.image.url} alt={slice.primary.image.alt} />
+        ) : null}
+      </div>
     </div>
-    { slice?.items?.map((item, i) => <div key={i} className={`gallery ${slice.variation}-gallery`}><RichText render={item.heading} /><img src={item.image.url} alt={item.image.alt} /> <span >{ item.test }</span></div>) }
-    {/* { slice?.items?.map((item, i) => <img src={item.image.url} alt={item.image.alt} />) }   */}
-    </section>
+  </section>
 );
 
 export default MySlice;

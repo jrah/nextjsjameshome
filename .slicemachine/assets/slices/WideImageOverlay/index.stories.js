886b195a17e7568fcd0602acccea6f33046d8506
James Home <james@jameshome.co.uk>
2021-07-30 21:25:45 +0100

    Replace svg icon

--- .slicemachine/assets/slices/WideImageOverlay/index.stories.js
+++ .slicemachine/assets/slices/WideImageOverlay/index.stories.js
@@ -1,9 +1,46 @@
-import MyComponent from '../../../../slices/WideImageOverlay';
+import MyComponent from "../../../../slices/WideImageOverlay";
 
 export default {
-  title: 'slices/WideImageOverlay'
-}
+  title: "slices/WideImageOverlay",
+};
 
-
-export const _DefaultSlice = () => <MyComponent slice={{"variation":"default-slice","name":"Default slice","slice_type":"wide_image_overlay","items":[],"primary":{"title":[{"type":"heading1","text":"Optimize robust web services","spans":[]}],"description":[{"type":"paragraph","text":"Qui irure eiusmod do elit ea excepteur magna fugiat sit. Veniam cillum laboris adipisicing irure minim sit aute fugiat cillum mollit pariatur. Commodo adipisicing ipsum quis irure aute laboris id anim elit eiusmod magna.","spans":[]},{"type":"paragraph","text":"Dolore proident nostrud dolor amet magna. Ullamco fugiat tempor ullamco proident eiusmod tempor officia et anim nostrud.","spans":[]}],"image":{"dimensions":{"width":900,"height":500},"alt":"Placeholder image","copyright":null,"url":"https://jameshome.cdn.prismic.io/jameshome/3b18dc11-7641-4688-b2a6-d6b48b202b1a_undraw_My_documents_re_13dc.svg?w=900&h=500&fit=crop"},"backgroundcolor":"#697689","shade":"light"},"id":"_DefaultSlice"}} />
-_DefaultSlice.storyName = 'Default slice'
+export const _DefaultSlice = () => (
+  <MyComponent
+    slice={{
+      variation: "default-slice",
+      name: "Default slice",
+      slice_type: "wide_image_overlay",
+      items: [],
+      primary: {
+        title: [
+          { type: "heading1", text: "Optimize robust web services", spans: [] },
+        ],
+        description: [
+          {
+            type: "paragraph",
+            text:
+              "Qui irure eiusmod do elit ea excepteur magna fugiat sit. Veniam cillum laboris adipisicing irure minim sit aute fugiat cillum mollit pariatur. Commodo adipisicing ipsum quis irure aute laboris id anim elit eiusmod magna.",
+            spans: [],
+          },
+          {
+            type: "paragraph",
+            text:
+              "Dolore proident nostrud dolor amet magna. Ullamco fugiat tempor ullamco proident eiusmod tempor officia et anim nostrud.",
+            spans: [],
+          },
+        ],
+        image: {
+          dimensions: { width: 900, height: 500 },
+          alt: "Placeholder image",
+          copyright: null,
+          url:
+            "https://jameshome.cdn.prismic.io/jameshome/71b19ee2-626b-41a3-992b-93b3a5e0f745_drawkit-daily-life-vector-illustration-02.svg?w=900&h=500&fit=crop",
+        },
+        backgroundcolor: "#697689",
+        shade: "light",
+      },
+      id: "_DefaultSlice",
+    }}
+  />
+);
+_DefaultSlice.storyName = "Default slice";

41d6173a28f26229fc88676a973da7c6e5a22b51
James Home <james@jameshome.co.uk>
2021-08-01 20:21:52 +0100

    Add basic navigation bar

--- supplementary/NavigationBar/index.js
+++ supplementary/NavigationBar/index.js
@@ -1,31 +1,19 @@
-import React from 'react'
-import { RichText } from 'prismic-reactjs'
+import React from "react";
+import { Link } from "prismic-reactjs";
 
 const NavigationBar = ({ slice }) => (
   <section>
-    <span className="title">
-      {
-        slice.primary.title ?
-        <RichText render={slice.primary.title}/>
-        : <h2>Template slice, update me!</h2>
-      }
-    </span>
-    {
-      slice.primary.description ?
-      <RichText render={slice.primary.description}/>
-      : <p>start by editing this slice from inside Prismic builder!</p>
-    }
-    <style jsx>{`
-        section {
-          max-width: 600px;
-          margin: 4em auto;
-          text-align: center;
-        }
-        .title {
-          color: #8592e0;
-        }
-    `}</style>
+    <nav class="flex items-center">
+    <img
+      className="w-36"
+      src={slice.primary.logo.url}
+      alt={slice.primary.logo.alt}
+    />
+    {slice?.items?.map((item, i) => (
+      <a className="block ml-4 " href={Link.url(item.linkurl)}>{item.linktext}</a>
+    ))}
+    </nav>
   </section>
-)
+);
 
-export default NavigationBar
+export default NavigationBar;

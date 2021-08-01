d5cf50de0135810443262d90caf7984334d55201
James Home <james@jameshome.co.uk>
2021-08-01 17:13:36 +0100

    Add menu

---
+++ .slicemachine/assets/supplementary/NavigationBar/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../supplementary/NavigationBar';
+
+export default {
+  title: 'supplementary/NavigationBar'
+}
+
+
+export const _DefaultSlice = () => <MyComponent slice={{"variation":"default-slice","name":"Default slice","slice_type":"navigation_bar","items":[{"linkurl":{"link_type":"Web","url":"http://twitter.com"},"linktext":"seize virtual infrastructures"},{"linkurl":{"link_type":"Web","url":"https://prismic.io"},"linktext":"aggregate enterprise e-markets"},{"linkurl":{"link_type":"Web","url":"http://google.com"},"linktext":"aggregate back-end schemas"},{"linkurl":{"link_type":"Web","url":"https://slicemachine.dev"},"linktext":"implement cross-media synergies"},{"linkurl":{"link_type":"Web","url":"http://twitter.com"},"linktext":"reintermediate dynamic applications"},{"linkurl":{"link_type":"Web","url":"http://google.com"},"linktext":"e-enable transparent web services"}],"primary":{"logo":{"dimensions":{"width":900,"height":500},"alt":"Placeholder image","copyright":null,"url":"https://jameshome.cdn.prismic.io/jameshome/ec8e21e5-7c68-4827-9f33-4c4fa3ec8f24_logoipsum-logo-7.svg?w=900&h=500&fit=crop"}},"id":"_DefaultSlice"}} />
+_DefaultSlice.storyName = 'Default slice'

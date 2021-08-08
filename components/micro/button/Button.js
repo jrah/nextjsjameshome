5df80803d4aee7832f8544454a5135f14c403feb
James Home <james@jameshome.co.uk>
2021-08-08 22:55:13 +0100

    Check undefined

--- components/micro/button/Button.js
+++ components/micro/button/Button.js
@@ -8,7 +8,7 @@ function Button({ children, type }) {
         className={classnames(
           {
             "text-white bg-indigo-600 hover:bg-indigo-700": type === "primary",
-            " text-indigo-700 bg-indigo-100 hover:bg-indigo-200": (type === null) | type === undefined,
+            "text-indigo-700 bg-indigo-100 hover:bg-indigo-200": type === undefined,
           },
           "inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md"
         )}

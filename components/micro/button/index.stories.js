6949114d8e1d5f125927190faa1258231c21b8cf
James Home <james@jameshome.co.uk>
2021-08-09 02:55:34 +0100

    Add svg example

--- components/micro/button/index.stories.js
+++ components/micro/button/index.stories.js
@@ -9,4 +9,4 @@ export default {
 
 export const DefaultButton = () => <Button>Default button</Button>
 export const PrimaryButton = () => <Button type="primary">Primary button</Button>
-export const AlternateButton = () => <Button type="alternate" outline>Primary button</Button>
+export const AlternateButton = () => <Button type="alternate" outline>Primary button <svg class="w-6 h-6 ml-2 -mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"></path></svg></Button>

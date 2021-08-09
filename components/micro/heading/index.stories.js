e2226d936995ff048e63593bdbcfa610d1428b18
James Home <james@jameshome.co.uk>
2021-08-09 15:34:57 +0100

    Add story for heading

---
+++ components/micro/heading/index.stories.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+import  Heading from "./Heading";
+
+export default {
+  title: "Micro/Heading",
+  component: Heading
+};
+
+export const DefaultHeading = () => <Heading>Default heading</Heading>
